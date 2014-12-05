3f213fd53229b184ab4bcbf6416828c112d4a784
adolenc <andrej.dolenc@student.uni-lj.si>
2014-12-05 10:28:35 +0100

    extracted complex numbers into separate file

---
+++ Src/Data/KnotComplex.hs
@@ -0,0 +1,33 @@
+module Src.Data.KnotComplex where
+
+import Prelude
+import Data.Ratio hiding (numerator,denominator)
+
+data K = K { re2::Rational, im2::Rational } deriving (Eq,Show)
+
+instance Num a => Num (Complex a) where
+  (a :+ b) + (a' :+ b') = (a+a') :+ (b+b')
+  (a :+ b) * (a' :+ b') = (a*a'-b*b') :+ (a*b'+a'*b)
+  negate (a :+ b) = (-a) :+ (-b)
+  fromInteger n = fromInteger n :+ 0
+  abs (a :+ b) = undefined
+  signum (a :+ b) = undefined
+
+instance Fractional a => Fractional (Complex a) where
+  recip (a :+ b) = let r = recip (a*a+b*b) in ((a*r) :+ (-b*r))
+  fromRational q = fromRational q :+ 0
+  
+
+instance Num K where
+  K a b + K a' b' = K (a+a') (b+b')
+  K a b * K a' b' = K (a*a'+2*b*b') (a*b'+a'*b)
+  negate (K a b) = K (negate a) (negate b)
+  abs _ = error ""
+  signum _ = error ""
+  fromInteger i = K (fromInteger i) 0
+
+instance Fractional K where
+  recip (K a b) = let r = recip (a*a-2*b) in K (r*a) (-r*b)
+  fromRational x = K x 0
+
+data Complex a = (:+) { realPart :: a, imagPart :: a } deriving (Eq,Show)
--- main.hs
+++ main.hs
@@ -1,28 +1,13 @@
 module Main where
 
 import Src.Control.Monad.VectorSpace
+import Src.Data.KnotComplex as KC
 import Prelude
 import Data.Ratio hiding (numerator,denominator)
 
-
-type KnotMonad = V L
 type L = Complex K
+type KnotMonad = V L
 
-data K = K { re2::Rational, im2::Rational } deriving (Eq,Show)
-
-instance Num K where
-  K a b + K a' b' = K (a+a') (b+b')
-  K a b * K a' b' = K (a*a'+2*b*b') (a*b'+a'*b)
-  negate (K a b) = K (negate a) (negate b)
-  abs _ = error ""
-  signum _ = error ""
-  fromInteger i = K (fromInteger i) 0
-
-instance Fractional K where
-  recip (K a b) = let r = recip (a*a-2*b) in K (r*a) (-r*b)
-  fromRational x = K x 0
-
-data Complex a = (:+) { realPart :: a, imagPart :: a } deriving (Eq,Show)
 
 hr2 = K 0 (1%2)
 a = hr2 :+ hr2
@@ -31,18 +16,6 @@ i = 0 :+ 1
 
 type Tangle = (Bool,Bool) -> KnotMonad (Bool,Bool)
 
-instance Num a => Num (Complex a) where
-  (a :+ b) + (a' :+ b') = (a+a') :+ (b+b')
-  (a :+ b) * (a' :+ b') = (a*a'-b*b') :+ (a*b'+a'*b)
-  negate (a :+ b) = (-a) :+ (-b)
-  fromInteger n = fromInteger n :+ 0
-  abs (a :+ b) = undefined
-  signum (a :+ b) = undefined
-
-instance Fractional a => Fractional (Complex a) where
-  recip (a :+ b) = let r = recip (a*a+b*b) in ((a*r) :+ (-b*r))
-  fromRational q = fromRational q :+ 0
-  
 cup :: (Bool,Bool) -> KnotMonad ()
 cup (u,v) = case (u,v) of
   (False,True) -> (-i * b) .* return ()
@@ -66,21 +39,21 @@ under (u,v) = b .* do
   .+
   a .* return (u,v)
   
-value :: Tangle -> Rational
-value p =
-  let (a,b) = value' p
-  in re2 $ realPart $ -i*a/b
-  
-value' p =
-  let alpha = coefficient (False,False) $ p (False,False)
-      beta  = coefficient (True,False)  $ p (False,True)
-  in (alpha,beta)
-
 infinity (i,j) = return (i,j)
 zero (i,j) = do
   cup (i,j)
   cap ()
 
+value' p =
+  let alpha = coefficient (False,False) $ p (False,False)
+      beta  = coefficient (True,False)  $ p (False,True)
+  in (alpha,beta)
+
+value :: Tangle -> Rational
+value p =
+  let (a,b) = value' p
+  in re2 $ realPart $ -i*a/b
+  
 steps :: Rational -> [String]
 steps 0 = []
 steps q | q<= -1 = "twist":steps (q+1)
@@ -88,6 +61,7 @@ steps q | q<= -1 = "twist":steps (q+1)
         | q>=1 = "antitwist":steps (q-1)
 
 untangle t = steps (value t)
+
 
 example :: Tangle
 example (a,b) = do
